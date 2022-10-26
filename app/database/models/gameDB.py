4aac86146a3f68ec625a2c67a8d720fdc1936d77
bgseong <qnrms2898@naver.com>
2022-10-27 07:01:00 +0900

    라우터 나누기 1차

--- app/database/models/gameDB.py
+++ app/database/models/gameDB.py
@@ -3,13 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from database.base import Base
 from .userDB import RiotUser
 import uuid
+from sqlalchemy.orm import relationship, backref
 
 
 class Game(Base):
     __tablename__= 'games'
 
     id=Column(String(120), primary_key=True, default=lambda: str(uuid.uuid4()))
-    user=Column(String(120), ForeignKey(RiotUser.riotuid))
+    user_ids = Column(String(120), ForeignKey(RiotUser.riotuid), nullable=True)
+    ids_user=relationship('RiotUser', foreign_keys=[user_ids], backref=backref('game_by_user',uselist=False))
     champion=Column(String(20), nullable=False)
     kill=Column(String(20), nullable=False)
     death=Column(String(20), nullable=False)
